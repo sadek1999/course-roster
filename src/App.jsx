97c6e4ed0c64b0f24e434ca51ba282966598feff
Sadek Ali <sadekali1530@gmail.com>
2023-09-15 00:08:43 +0600

    complit the ui for impliment  funshonality

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,15 @@ function App() {
   
 
   return (
-    <>
-      
+    <div className='bg-slate-200 p-4'>
+    
       <h1 className='text-4xl font-bold text-center'>Corse Registration</h1>
       <div className='flex gap-3 mt-3'>
         <Blogs></Blogs>
         <Head></Head>
       </div>
       
-    </>
+      </div>
   )
 }
 
--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,25 +1,25 @@
-import {  FaBeer } from 'react-icons/fa';
+import {  BiBookOpen } from 'react-icons/bi';
 
 
 const Blog = ({blog}) => {
     const {title ,img,description,price,cardthours}=blog;
     
     return (
-        <div className="bg-slate-200 m-2 rounded p-4 space-y-2">
+        <div className="bg-white m-2 rounded-lg p-4 space-y-2">
             <img className="mx-auto " src={img} alt="" />
             <h3 className="text-xl font-bold">{title}  </h3>
             <p className="">{description}</p>
-            <div className="flex justify-between">
+            <div className="flex justify-between p-2">
             <div >
                 
                 <p>price ={price}</p>
             </div>
             <div className='flex gap-2 items-center'>
-                <h2><FaBeer></FaBeer></h2>
+                <h2><BiBookOpen></BiBookOpen></h2>
                 <p>credit:{cardthours}</p>
             </div>
             </div>
-            <button className="bg-blue-400 text-white w-full text-xl py-1 rounded-lg ">select</button>
+            <button className="bg-blue-500 text-white w-full font-bold text-xl py-1 rounded-lg ">select</button>
             
         </div>
     );
